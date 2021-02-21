b58b6976f4d392afcf49ac39621db4e01874f5d4
JJ <jj.vega86@gmail.com>
2021-02-21 15:23:39 -0500

    Added props references to CommentDetail component

--- src/components/CommentDetail.js
+++ src/components/CommentDetail.js
@@ -1,20 +1,23 @@
 import React from "react";
 
-const CommentDetail = () => {
+
+const CommentDetail = (props) => {
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={getRandomImage()} />
+        <img alt="avatar" src={props.getRandomImage()} />
       </a>
       <div className="content">
         <a href="/" className="author">
-          Thomas
+          {props.author}
         </a>
         <div className="metadata">
-          <span className="date">Today at 6:00pm</span>
+          <span className="date">{props.dateTime}</span>
         </div>
-        <div className="text">Nice blog post!</div>
+        <div className="text">{props.commentText}</div>
       </div>
     </div>
   );
 };
+
+export default CommentDetail;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
+import CommentDetail from './components/CommentDetail';
 
 const getRandomImage = () => {
     let image = `${faker.image.people()}?random=${Math.round(Math.random() * 1000)}`;
@@ -11,48 +12,7 @@ const getRandomImage = () => {
 const App = () => {
     return (
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={getRandomImage()} />
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                    Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00pm</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={getRandomImage()} />
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                    Thomas
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00pm</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={getRandomImage()} />
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                    Susan
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00pm</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
+           
         </div>
     );
 }
